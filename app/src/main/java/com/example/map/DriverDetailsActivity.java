fdcdb077702ec8e3b28d32cd91b88b30abdf5826
sharanshanmughan <sharanshanmughan001@gmail.com>
2020-03-11 14:35:52 +0530

    threepageupdate

--- app/src/main/java/com/example/map/DriverDetailsActivity.java
+++ app/src/main/java/com/example/map/DriverDetailsActivity.java
@@ -2,13 +2,24 @@ package com.example.map;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class DriverDetailsActivity extends AppCompatActivity {
-
+    ImageView backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_details);
+        backButton=(ImageView)findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DriverDetailsActivity.this,TabBottomParent.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/map/ProfileActivity.java
+++ app/src/main/java/com/example/map/ProfileActivity.java
@@ -2,13 +2,24 @@ package com.example.map;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    ImageView backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        backButton=(ImageView)findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this,TabBottomParent.class);
+                startActivity(intent);
+            }
+        });
     }
 }
