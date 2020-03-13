08a61aaefa808291a0df94c7317f1f6e1da1e206
sharanshanmughan <sharanshanmughan001@gmail.com>
2020-03-13 13:43:14 +0530

    errorpopup

--- app/src/main/java/com/example/map/SchoolDetailsActivity.java
+++ app/src/main/java/com/example/map/SchoolDetailsActivity.java
@@ -3,21 +3,35 @@ package com.example.map;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class SchoolDetailsActivity extends AppCompatActivity {
     ImageView backButton;
+    RelativeLayout dialNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_school_details);
         backButton=(ImageView)findViewById(R.id.backButton);
+        dialNumber=(RelativeLayout)findViewById(R.id.dialNumber);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SchoolDetailsActivity.this,TabBottomParent.class);
+                startActivity(intent);
+            }
+        });
+        dialNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  Toast.makeText(SchoolDetailsActivity.this,"work",Toast.LENGTH_LONG).show();
+                Intent intent=new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:0123456789"));
                 startActivity(intent);
             }
         });
